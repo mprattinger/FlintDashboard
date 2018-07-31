22a11206437167941278f266bdbb1d19234418df
Michael Prattinger <mprattinger@outlook.com>
2018-07-31 13:32:10 +0200

    Part 4 working

--- site.js
+++ site.js
@@ -1,21 +1,9 @@
 $(document).ready(function () {
 
     $(document).ready(function () {
-        $("#sidebar").mCustomScrollbar({
-            theme: "minimal"
-        });
-
-        $('#dismiss, .overlay').on('click', function () {
-            $('#sidebar').removeClass('active');
-            $('.overlay').removeClass('active');
-        });
-
         $('#sidebarCollapse').on('click', function () {
-            $('#sidebar').addClass('active');
-            $('.overlay').addClass('active');
-            $('.collapse.in').toggleClass('in');
-            $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+            $('#sidebar').toggleClass('active');
         });
     });
-    
+
 });

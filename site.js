34fbc3f42b5756b6f4c33d5143cbaa54550c5a1c
Michael Prattinger <mprattinger@outlook.com>
2018-07-31 13:23:41 +0200

    Part 4

--- site.js
+++ site.js
@@ -1,16 +1,21 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    $("#sidebar").mCustomScrollbar({
-        theme: "minimal"
-   });
+    $(document).ready(function () {
+        $("#sidebar").mCustomScrollbar({
+            theme: "minimal"
+        });
 
-    $('#sidebarCollapse').on('click', function(){
+        $('#dismiss, .overlay').on('click', function () {
+            $('#sidebar').removeClass('active');
+            $('.overlay').removeClass('active');
+        });
 
-        $('#sidebar').toggleClass('active');
-        $('.list-unstyled.collapse.show').toggleClass('show');
-        // and also adjust aria-expanded attributes we use for the open/closed arrows
-        // in our CSS
-        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
-
+        $('#sidebarCollapse').on('click', function () {
+            $('#sidebar').addClass('active');
+            $('.overlay').addClass('active');
+            $('.collapse.in').toggleClass('in');
+            $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+        });
     });
+    
 });

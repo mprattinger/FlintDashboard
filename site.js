8d1e6282dbe0b5d154d8645c8d1ac5185dc0b0b1
Michael Prattinger <mprattinger@outlook.com>
2018-07-31 15:00:22 +0200

    Working draft

--- site.js
+++ site.js
@@ -1,9 +1,30 @@
-$(document).ready(function () {
-
     $(document).ready(function () {
+
+        $('[data-toggle="tooltip"]').tooltip();
+
+        $("#sidebar").mCustomScrollbar({
+            theme: "minimal"
+        });
+
         $('#sidebarCollapse').on('click', function () {
             $('#sidebar').toggleClass('active');
+            
         });
-    });
 
-});
+        $('#dismiss').on('click', function () {
+            $('#sidebar').toggleClass('active');
+            $('#content').toggleClass('active');
+
+            $('.menuText').toggleClass('hide-Menu-Text');
+            if ($('.dismiss-arrow').hasClass('fa-arrow-left')) {
+                //fa-arrow-left
+                $('.dismiss-arrow').removeClass('fa-arrow-left');
+                $('.dismiss-arrow').addClass('fa-arrow-right');
+                
+            }else{
+                $('.dismiss-arrow').addClass('fa-arrow-left');
+                $('.dismiss-arrow').removeClass('fa-arrow-right');
+            }
+        });
+
+    });
